831f58e54af84dceff26141ddd1a0b2daa0b717e
Zulfiqar Ahmed <zulfiqarahmed.official93@gmail.com>
2026-08-09 20:44:54 +0200

    Initial Commit

---
+++ khelo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  khelo
+//
+//  Created by Zulfiqar Ahmed on 09.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ khelo/kheloApp.swift
@@ -0,0 +1,17 @@
+//
+//  kheloApp.swift
+//  khelo
+//
+//  Created by Zulfiqar Ahmed on 09.08.26.
+//
+
+import SwiftUI
+
+@main
+struct kheloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
